f6df86512f923463932cb91cf7aaa77c054f4e20
tbrekalo <t@tbrekalo.xyz>
2025-11-01 20:35:05 +0000

    tbrekalo-fmt: update example

--- example.cc
+++ example.cc
@@ -2,20 +2,9 @@
 
 auto main(int, char**) -> int {
   namespace tb = tbrekalo;
-  auto run = [](auto& logger) -> void {
-    tb::log::trace<"{}">(logger)(42);
-    tb::log::debug<"{}">(logger)(42);
-    tb::log::info<"{}">(logger)(42);
-    tb::log::warn<"{}">(logger)(42);
-    tb::log::error<"{}">(logger)(42);
-    tb::log::fatal<"{}">(logger)(42);
-  };
+  tb::log::trace(tb::log::TTY_SINK)("{}", 42);
+  tb::log::debug(tb::log::TTY_SINK)("{}", 1337);
+  tb::log::info(tb::log::TTY_SINK)("{}", "HelloWorld");
 
-  tb::log::FileLogger tty(stdout, tb::log::Level::TRACE, tb::log::FmtLevelTty);
-  tb::log::FileLogger file("/tmp/log.txt", tb::log::Level::TRACE, tb::log::FmtLevelASCII);
-
-  run(tty);
-  run(file);
-
-  return 0;
+  return EXIT_SUCCESS;
 }
