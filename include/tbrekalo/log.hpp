64172934835abd3dd06e184f31dec4e651c18643
tbrekalo <t@tbrekalo.xyz>
2025-11-01 20:32:30 +0000

    tbrekalo-fmt: record based logging

--- include/tbrekalo/log.hpp
+++ include/tbrekalo/log.hpp
@@ -1,46 +1,57 @@
 #ifndef TBREKALO_LOG_HPP_
 #define TBREKALO_LOG_HPP_
 
+#include <unistd.h>
+
+#include <array>
 #include <chrono>
-#include <filesystem>
+#include <cstdlib>
 #include <print>
 #include <source_location>
-#include <string_view>
 #include <thread>
-
-namespace tbrekalo::log::detail {
-
-template <size_t N>
-struct FixedString {
-  char data[N + 1];
-  consteval FixedString() noexcept : data{'\0'} {}
-  consteval FixedString(char const (&src)[N + 1]) noexcept {
-    __builtin_memcpy(data, src, N + 1);
-  }
-
-  static constexpr auto size() -> size_t {
-    return N;
-  }
-};
-
-template <size_t N>
-FixedString(char const (&data)[N]) -> FixedString<N - 1>;
-
-template <size_t Lhs, size_t Rhs>
-consteval auto operator+(FixedString<Lhs> lhs, FixedString<Rhs> rhs) -> FixedString<Lhs + Rhs> {
-  FixedString<Lhs + Rhs> dst;
-  __builtin_memcpy(dst.data, lhs.data, Lhs);
-  __builtin_memcpy(dst.data + Lhs, rhs.data, Rhs + 1);
-  return dst;
-}
-
-} // namespace tbrekalo::log::detail
+#include <type_traits>
+#include <utility>
 
 namespace tbrekalo::log {
 
-enum class Level { TRACE, DEBUG, INFO, WARN, ERROR, FATAL };
+enum class Level : uint8_t { TRACE, DEBUG, INFO, WARN, ERROR, FATAL };
 
-inline constexpr auto FmtLevelTty(Level level) noexcept -> std::string_view {
+struct Record {
+  std::chrono::system_clock::time_point ts;
+  std::source_location loc;
+  std::thread::id thread;
+  std::string msg;
+  Level level;
+};
+
+template <Level level>
+inline constexpr auto log =
+    []<class SinkT> [[gnu::always_inline]] (SinkT&& sink, std::source_location loc = std::source_location::current())
+  requires(std::is_lvalue_reference_v<SinkT> && std::is_invocable_r_v<void, SinkT, Record>)
+{
+  return [sink = std::forward<decltype(sink)>(sink),
+          loc]<class... Args> [[gnu::always_inline]] (std::format_string<Args...> fmt, Args&&... args) {
+    sink(Record{.ts = std::chrono::system_clock::now(),
+                .loc = loc,
+                .msg = std::format(fmt, std::forward<Args>(args)...),
+                .level = level});
+  };
+};
+
+/* clang-format off */
+inline constexpr auto trace = log<Level::TRACE>;
+inline constexpr auto debug = log<Level::DEBUG>;
+inline constexpr auto info  = log<Level::INFO>;
+inline constexpr auto warn  = log<Level::WARN>;
+inline constexpr auto error = log<Level::ERROR>;
+inline constexpr auto fatal = log<Level::FATAL>;
+/* clang-format on */
+
+inline static Level ENV_LEVEL = [](auto val) static -> Level {
+  return std::to_underlying(Level::TRACE) <= val && val <= std::to_underlying(Level::WARN) ? Level(val) : Level::INFO;
+}([](auto str) { return str != nullptr ? std::atoi(str) : std::to_underlying(Level::INFO); }(::getenv("TB_LOG_LEVEL")));
+
+inline constexpr auto TTY_SINK = [](Record record) static {
   static constexpr std::array REPRS{
       "\033[37mTRACE\033[0m\t",  // White
       "\033[36mDEBUG\033[0m\t",  // Cyan
@@ -50,67 +61,15 @@ inline constexpr auto FmtLevelTty(Level level) noexcept -> std::string_view {
       "\033[1;31mFATAL\033[0m\t" // Bold Red
   };
 
-  return REPRS[std::to_underlying(level)];
-}
+#ifndef TB_LOG_LEVEL
+#define TB_LOG_LEVEL ENV_LEVEL
+#endif
 
-inline constexpr auto FmtLevelASCII(Level level) noexcept -> std::string_view {
-  static constexpr std::array REPRS{"TRACE\t", "DEBUG\t", "INFO\t", "WARN\t", "ERROR\t", "FATAL\t"};
-  return REPRS[std::to_underlying(level)];
-}
-
-class FileLogger {
-  static constexpr auto NO_OPT_DELETER = +[](std::FILE*) {};
-  static constexpr auto FCLOSE_DELETER = +[](std::FILE* file) { std::fclose(file); };
-
-  std::unique_ptr<std::FILE, void (*)(std::FILE*)> file_;
-
-  Level level_;
-  auto (*fmt_level_)(Level level) noexcept -> std::string_view;
-
-public:
-  using LevelFmtFn = auto (*)(Level) noexcept -> std::string_view;
-
-  FileLogger(std::FILE* file, Level level, LevelFmtFn fmt_level)
-      : file_(file, NO_OPT_DELETER), level_(level), fmt_level_(fmt_level) {}
-  FileLogger(std::filesystem::path const& file, Level level, LevelFmtFn fmt_level) noexcept
-      : file_(std::fopen(file.c_str(), "a"), FCLOSE_DELETER), level_(level), fmt_level_(fmt_level) {}
-
-  auto level() const noexcept -> Level {
-    return level_;
-  }
-
-  auto level(Level level) noexcept -> void {
-    level_ = level;
-  }
-
-  template <detail::FixedString Fmt, class... Args>
-  [[gnu::always_inline]] auto log(auto time_point, std::thread::id thread_id, Level level, std::string_view file_name,
-                                  int line, Args&&... args) const -> void {
-    static constexpr auto fmt = detail::FixedString("{:%FT%TZ} {} {} {}:{} ") + Fmt + detail::FixedString("\n");
-    if (level >= level_) [[likely]] {
-      std::print(file_.get(), fmt.data, time_point, thread_id, fmt_level_(level), file_name, line,
-                 std::forward<Args>(args)...);
-    }
+  if (record.level >= TB_LOG_LEVEL) [[likely]] {
+    std::print("{:%FT%T%z}\t{}\t{}\t{}\t{}:{}\t'{}'\n", record.ts, getpid(), record.thread,
+               REPRS[std::to_underlying(record.level)], record.loc.file_name(), record.loc.line(), record.msg);
   }
 };
-
-template <detail::FixedString Fmt, Level Level = Level::INFO>
-inline constexpr auto log =
-    [] [[gnu::always_inline]] (auto& sink, std::source_location loc = std::source_location::current()) {
-      return [&sink, loc]<class... Args> [[gnu::always_inline]] (Args&&... args) {
-        sink.template log<Fmt, Args...>(std::chrono::system_clock::now(), std::this_thread::get_id(), Level,
-                                        loc.file_name(), loc.line(), std::forward<Args>(args)...);
-      };
-    };
-
-/* clang-format off */
-template <detail::FixedString Fmt> inline constexpr auto trace = log<Fmt, Level::TRACE>;
-template <detail::FixedString Fmt> inline constexpr auto debug = log<Fmt, Level::DEBUG>;
-template <detail::FixedString Fmt> inline constexpr auto info  = log<Fmt, Level::INFO >;
-template <detail::FixedString Fmt> inline constexpr auto warn  = log<Fmt, Level::WARN >;
-template <detail::FixedString Fmt> inline constexpr auto error = log<Fmt, Level::ERROR>;
-template <detail::FixedString Fmt> inline constexpr auto fatal = log<Fmt, Level::FATAL>;
-/* clang-format on */
 
 } // namespace tbrekalo::log
 
