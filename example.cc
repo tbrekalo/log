e4cb1d7170162245c25185b2a062c33d7340f30e
tbrekalo <t@tbrekalo.xyz>
2025-11-18 22:42:36 +0000

    tbrekalo-fmt: update example

--- example.cc
+++ example.cc
@@ -2,9 +2,17 @@
 
 auto main(int, char**) -> int {
   namespace tb = tbrekalo;
-  tb::log::trace(tb::log::TTY_SINK)("{}", 42);
-  tb::log::debug(tb::log::TTY_SINK)("{}", 1337);
-  tb::log::info(tb::log::TTY_SINK)("{}", "HelloWorld");
+  auto sink = [file = std::unique_ptr<std::FILE, decltype([](std::FILE* f) -> void { std::fclose(f); })>(
+                   std::fopen("/tmp/tb-log.txt", "a"))](tb::log::Record record) -> void {
+    tb::log::TTY_SINK(record);
+    std::print(file.get(), "{:%FT%T%z}\t{}\t{}\t{}:{}\t'{}'\n", record.ts, record.pid, std::to_underlying(record.level),
+               record.loc.file_name(), record.loc.line(), record.msg);
+    std::fflush(file.get());
+  };
+
+  tb::log::trace(sink)("{}", 42);
+  tb::log::debug(sink)("{}", 1337);
+  tb::log::info(sink)("{}", "HelloWorld");
 
   return EXIT_SUCCESS;
 }
